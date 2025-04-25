fe16ed979eb8424539f3f0ef88b3d4329ddf909e
SunhieshkaOptisol <sunhieshka.c@optisolbusiness.com>
2025-04-25 17:38:58 +0530

    commit

--- elsai_core/extractors/__init__.py
+++ elsai_core/extractors/__init__.py
@@ -4,7 +4,6 @@ from .unstructured_excel_loader_service import UnstructuredExcelLoaderService
 from .azure_cognitive_service import AzureCognitiveService
 from .azure_document_intelligence import AzureDocumentIntelligence
 from .csv_file_extractor import CSVFileExtractor
-from .docling_service import DoclingPDFTextExtractor
 from .docx_text_extractor import DocxTextExtractor
 from .pypdfloader_service import PyPDFTextExtractor
 from .visionai_pdf_extractor import VisionAIExtractor
@@ -16,7 +15,6 @@ __all__ = [
     AzureCognitiveService,
     AzureDocumentIntelligence,
     CSVFileExtractor,
-    DoclingPDFTextExtractor,
     DocxTextExtractor,
     PyPDFTextExtractor,
     VisionAIExtractor
